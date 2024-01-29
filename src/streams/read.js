32fad5d899d761f491bdefa6b09d058e3eb2eac7
Gregory Pereverzev <boot762@gmail.com>
2024-01-29 21:13:41 +0300

    feat: implement streams/read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,21 @@
+import fs from 'fs';
+
 const read = async () => {
-    // Write your code here 
+  const FILE = 'src/streams/files/fileToRead.txt';
+
+  const readableStream = fs.createReadStream(FILE, { encoding: 'utf-8' });
+
+  readableStream.on('data', chunk => {
+    process.stdout.write(chunk);
+  });
+
+  readableStream.on('end', () => {
+    console.log('\nFile read successfully.');
+  });
+
+  readableStream.on('error', error => {
+    console.error(error);
+  });
 };
 
 await read();
