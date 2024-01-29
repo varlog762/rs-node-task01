03c6e70bd5192280e8937c3c58162ffc923520c0
Gregory Pereverzev <boot762@gmail.com>
2024-01-30 00:20:26 +0300

    feat: implement calcHash, compress & decompress functions

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,24 @@
+import fs from 'fs';
+import crypto from 'crypto';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const FILE = 'src/hash/files/fileToCalculateHashFor.txt';
+
+  const hash = crypto.createHash('sha256');
+  const readStream = fs.createReadStream(FILE);
+
+  readStream.on('data', chunk => {
+    hash.update(chunk);
+  });
+
+  readStream.on('end', () => {
+    const hexHash = hash.digest('hex');
+    console.log('SHA256 Hash:', hexHash);
+  });
+
+  readStream.on('error', error => {
+    console.error(error);
+  });
 };
 
 await calculateHash();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,31 @@
+import fs from 'fs';
+import zlib from 'zlib';
+
 const decompress = async () => {
-    // Write your code here 
+  const SOURCE_FILE = 'src/zip/files/archive.gz';
+  const DESTINATION_FILE = 'src/zip/files/fileToCompress.txt';
+
+  const readStream = fs.createReadStream(SOURCE_FILE);
+  const decompressor = zlib.createGunzip();
+  const writeStream = fs.createWriteStream(DESTINATION_FILE);
+
+  readStream.pipe(decompressor).pipe(writeStream);
+
+  readStream.on('error', error => {
+    console.error('Error reading compressed file:', error);
+  });
+
+  readStream.on('error', error => {
+    console.error(error);
+  });
+
+  decompressor.on('error', error => {
+    console.error(error + '1');
+  });
+
+  writeStream.on('error', error => {
+    console.error(error + '2');
+  });
 };
 
 await decompress();
